fb4faba1541b068f7c557576934d4aa9e871d9fd
omccarth <omccarth333@gmail.com>
2019-09-05 18:54:27 -0400

    Got single word generation done

--- Project CRYPTONYM.py
+++ Project CRYPTONYM.py
@@ -4,11 +4,13 @@ import os
 file = open('Prefixes.json', 'r+')
 prefixes = json.load(file)
 
-'''
-Add a prefix to the list. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
-The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
-For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
-'''
+wordsFile = open('words_dictionary.json', 'r')
+words = json.load(wordsFile)
+
+
+#Add a prefix to the list. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
+#The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
+#For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
 def addPrefix(category, prefix):
     if isinstance(category, str) and isinstance(prefix, str) and len(prefix) == 2:
         prefixes[category] = prefix
@@ -18,12 +20,10 @@ def addPrefix(category, prefix):
         file = open('Prefixes.json', 'w+')
         json.dump(prefixes, file, indent=4)
 
-'''
-Change a prefix on the list. Include the old category and the program will find that prefix and set the category and prefix to the second set of 
-inputs. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
-The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
-For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
-'''
+#Change a prefix on the list. Include the old category and the program will find that prefix and set the category and prefix to the second set of 
+#inputs. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
+#The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
+#For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
 def editPrefix(oldCategory, newCategory, newPrefix):
     removePrefix(oldCategory)
     addPrefix(newCategory, newPrefix)
@@ -33,11 +33,10 @@ def editPrefix(oldCategory, newCategory, newPrefix):
     file = open('Prefixes.json', 'w+')
     json.dump(prefixes, file, indent=4)
     
-'''
-Remove a prefix from the list. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
-The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
-For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
-'''
+
+#Remove a prefix from the list. The category should be a word or phrase in the form of a string that describes the type of project that would use this prefix.
+#The prefix should be two capital letters in the form of a string that fit the category and are a common start for words. Ex: Category; Python, Prefix; PI.
+#For python the prefix could have been py, but there aren't as many words that start with py as there are that start with pi.
 def removePrefix(category):
     global file
     if category in prefixes:
@@ -49,6 +48,24 @@ def removePrefix(category):
     else:
         return 'Category not found'
  
-removePrefix("DELETE")
-removePrefix("Temp")
+
+#Generate a list of project names from a two capital letters in the form of a string. These project names will be any words that start with the prefix. outputFilePath should be a string.
+#This will output a long list on names like Project PIANO and Project PICTRUEFRAME (in the case of a PI prefix for python), if you want names like PISTORM and PICAMERA, use generateWordNAME()
+def generateNameList(prefix, outputFileName):
+    global words
+    f = open(outputFileName, 'w+')
+    count = 0
+    output = {}
+    if isinstance(prefix, str) and len(prefix) == 2:
+        for word in words:
+            lowerPrefix = prefix.lower()
+            if isinstance(word, str) and word[0:2] == lowerPrefix[0:2]:
+                count += 1
+                caps = word.upper()
+                append = caps[2:len(caps)]
+                output[count] = ("Project " + prefix + append)
+    json.dump(output, f, indent=4)
+generateNameList("PI", 'Output.json')
+
+wordsFile.close
 file.close()
